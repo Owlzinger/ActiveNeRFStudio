951f1ba6a961a2cead255abe033c3efc09cff8ac
aneins <zengxin.jiao@tum.de>
2023-12-19 16:32:11 +0100

    编辑template

--- method_template/template_field.py
+++ method_template/template_field.py
@@ -24,11 +24,44 @@ class TemplateNerfField(NerfactoField):
     aabb: Tensor
 
     def __init__(
-        self,
-        aabb: Tensor,
-        num_images: int,
+            self,
+            position_encoding: Encoding = Identity(in_dim=3),
+            direction_encoding: Encoding = Identity(in_dim=3),
+            base_mlp_num_layers: int = 8,
+            base_mlp_layer_width: int = 256,
+            # TODO
+            head_mlp_num_layers: int = 2,
+            # TODO
+            head_mlp_layer_width: int = 128,
+            skip_connections: Tuple[int] = (4,),
+            field_heads: Optional[Tuple[Type[FieldHead]]] = (RGBFieldHead,),
+            use_integrated_encoding: bool = False,
+            spatial_distortion: Optional[SpatialDistortion] = None,
     ) -> None:
-        super().__init__(aabb=aabb, num_images=num_images)
+        super().__init__()
+        self.position_encoding = position_encoding
+        self.direction_encoding = direction_encoding
+        self.use_integrated_encoding = use_integrated_encoding
+        self.spatial_distortion = spatial_distortion
 
+        self.mlp_base = MLP(
+            in_dim=self.position_encoding.get_out_dim(),
+            num_layers=base_mlp_num_layers,
+            layer_width=base_mlp_layer_width,
+            skip_connections=skip_connections,
+            out_activation=nn.ReLU(),
+        )
+        self.mlp_head = MLP(
+            in_dim=self.mlp_base.get_out_dim() + self.direction_encoding.get_out_dim(),
+            num_layers=head_mlp_num_layers,
+            layer_width=head_mlp_layer_width,
+            out_activation=nn.ReLU(),
+        )
+
+        self.field_output_density = DensityFieldHead(in_dim=self.mlp_base.get_out_dim())
+        self.field_heads = nn.ModuleList(
+            [field_head() for field_head in field_heads] if field_heads else [])  # type: ignore
+        for field_head in self.field_heads:
+            field_head.set_in_dim(self.mlp_head.get_out_dim())  # type: ignore
     # TODO: Override any potential methods to implement your own field.
     # or subclass from base Field and define all mandatory methods.
--- method_template/template_model.py
+++ method_template/template_model.py
@@ -10,14 +10,31 @@ from nerfstudio.models.nerfacto import NerfactoModel, NerfactoModelConfig  # for
 from nerfstudio.models.base_model import Model, ModelConfig  # for custom Model
 
 
+
 @dataclass
 class TemplateModelConfig(NerfactoModelConfig):
     """Template Model Configuration.
 
     Add your custom model config parameters here.
     """
-
+    """ActiveNeRF Model Config"""
+    # --config_lego.txt
     _target: Type = field(default_factory=lambda: ActiveNeRFModel)
+    # follow the settings in NeRF, and sample 64, 128 points
+    # for coarse and fine models respectively.
+    """Number of samples in coarse field evaluation"""
+    num_coarse_samples: int = 64
+    """Number of samples in fine field evaluation"""
+    num_importance_samples: int = 128
+    # following setting by default
+    enable_temporal_distortion: bool = False
+    """Specifies whether or not to include ray warping based on time."""
+    temporal_distortion_params: Dict[str, Any] = to_immutable_dict({"kind": TemporalDistortionKind.DNERF})
+    """Parameters to instantiate temporal distortion with"""
+    use_gradient_scaling: bool = False
+    """Use gradient scaler where the gradients are lower for points closer to the camera."""
+    background_color: Literal["random", "last_sample", "black", "white"] = "white"
+    """Whether to randomize the background color."""
 
 
 class TemplateModel(NerfactoModel):
