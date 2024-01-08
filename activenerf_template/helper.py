8aeb624349f5c13e355396fe8fc125cc559b1b3b
aneins <zengxin.jiao@tum.de>
2024-01-08 22:27:32 +0100

    MAC 进行的更改

--- activenerf_template/helper.py
+++ activenerf_template/helper.py
@@ -1,14 +1,6 @@
 import torch
-
-img2mse_uncert_alpha = (
-    lambda x, y, uncert, alpha, w: torch.mean((1 / (2 * (uncert + 1e-9).unsqueeze(-1))) * ((x - y) ** 2))
-    + 0.5 * torch.mean(torch.log(uncert + 1e-9))
-    + w * alpha.mean()
-    + 4.0
-)
-
-
-def get_uncertainty(self, x, y, uncert, alpha, w):
+import os
+def get_uncertainty(x, y, uncert, alpha, w):
     return (
         torch.mean((1 / (2 * (uncert + 1e-9).unsqueeze(-1))) * ((x - y) ** 2))
         + 0.5 * torch.mean(torch.log(uncert + 1e-9))
